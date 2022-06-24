7406fda4dbb585a9d2cfadcb11c11a08e30426e4
software <software@umuts-MBP.home>
2022-06-24 14:09:55 +0300

    server oluşturuldu

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const topla = (par1, par2) => {
-    return par1+par2
-}
+const express = require("express")
+const app = express()
+/* host and port */
+const hostname = "localhost"
+const port = 3000
 
-console.log(topla(5,10));
+app.get("/", (req,res) => {
+    res.send("hello world | site is open")
+})
+
+// about page
+app.get("/about", (req,res) => {
+    res.send("about page")
+})
+
+// contact page
+app.get("/contact", (req,res) => {
+    res.send("contact page")
+})
+
+app.listen(port, ()=>{
+    console.log(`server is online: http://${hostname}:${port}`)
+})
