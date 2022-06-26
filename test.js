4df3d8061895fb0ea1d18f6d06a62affc78e5f57
software <software@umuts-MBP.home>
2022-06-26 16:07:54 +0300

    CRUD işlemleri gerçekleştirildi

--- test.js
+++ test.js
@@ -1,27 +1,58 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // database connection
 mongoose.connect("mongodb://localhost/my-database", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Schema created
 const UserSchema = new Schema({
-    name: String,
-    username: String,
-    mail: String,
-    password: String
-})
+  name: String,
+  username: String,
+  mail: String,
+  password: String,
+});
 
 // Model created
-const User = mongoose.model("User", UserSchema)
+const User = mongoose.model("User", UserSchema);
 
 // User created
-User.create({
+/* User.create({
     name: "Serhat Akdoğan",
     username: "serhatakdogan",
     mail: "serhatakdgn@gmail.com",
     password: "123345Serhat."
+}) */
+
+// read data
+/* User.find({}, (err, data) => {
+    if(!err) console.log(data);
+})
+ */
+
+// user 2 created
+/*User.create({
+    name: "Yusuf İslam Çiftci", 
+    username: "yusufislam",
+    mail: "yusufislam@gmail.com",
+    password: "islamdilan123."
+})*/
+
+const userId = "62b85552fb9ade25bea3aee7"; // serhat'ın id'si
+// update user
+
+/* User.findByIdAndUpdate(
+    userId,
+    {name: "Serhat AKDOĞAN"},
+    {new: true}, // update edildikten sonra aşağıda update edilen datanın anında gözümkesi için burası şart!
+    (err, data) => {
+        if(!err) console.log(data);
+    }
+) */
+
+// delete data
+User.findByIdAndDelete(userId, (err, data) => {
+    console.log(userId+ " id'sine sahip kayıt silindi [!]");
 })
