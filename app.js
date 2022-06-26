04f23c4567706f2c01a8d107169f26e3c208247b
software <software@umuts-MBP.home>
2022-06-26 16:50:30 +0300

    urlencoded ve json middlewareları oluşturuldu, create post çalıştırıldı ve post schema, model oluşturuldu

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
 const ejs = require("ejs");
 const hostname = "localhost";
 const port = 3000;
 const path = require("path");
+
+/* DATABASE CONNECTION */
+mongoose.connect("mongodb://localhost/pcat-blog")
+
+/* CREATED SCHEMA */
+const Schema = mongoose.Schema
 
 /* TEMPLATE ENGINE : EJS */
 
@@ -15,6 +22,12 @@ app.set("view engine", "ejs"); // ejs'yi template engine olarak kullanacağımı
 /* middleware'ler sırayla çalışırlar. Middleware use ile kullanılır ve next() metodunu
 kullanmazsak bir sonrakine geçemez diye not düşülsün */
 app.use(express.static("public"));
+// req.body undefined döndüğü için iki tane middleware aşağıdaki gibidir:
+// url'deki datayı okumamızı sağlıyor
+app.use(express.urlencoded({extended:true}))
+// url'deki datayı json formatında veriyor
+app.use(express.json())
+
 
 /* ROUTES */
 app.get("/", (req, res) => {
@@ -32,6 +45,34 @@ app.get("/add", (req, res) => {
   res.render("add");
 });
 
+// ADD PHOTO
+
+// photo schema
+const PhotoSchema = new Schema({
+  title: String,
+  description: String,
+  photoUrl: String
+})
+
+// photo model
+const Photo = mongoose.model("Photo", PhotoSchema)
+
+// Uploaded Photo
+app.post("/photos", (req,res) => {
+  Photo.create({
+    title: req.body.title,
+    description: req.body.description,
+    photoUrl: req.body.image
+  })
+  res.redirect("/") // işlemlerin bittikten sonra index sayfasına git
+})
+
+
+
+
+
 app.listen(port, () => {
   console.log(`server is online: http://${hostname}:${port}`);
 });
+
+
